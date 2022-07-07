d8a51355c900cee861b781ef139c4cddd89b2274
VladlinaS <108859782+VladlinaS@users.noreply.github.com>
2022-07-07 10:10:12 +0300

    Delete Years.py

--- Years.py
+++
@@ -1,8 +0,0 @@
-year_of_birth = input("Write the year of your birth")
-current_year  = 2022
-if year_of_birth > 1900 and 2022 <:
-    your_age = current_year - year_of_birth
-else: year_of_birth < 1900 and > 2022 :
-    print ("Error")
-
-
