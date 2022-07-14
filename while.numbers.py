015e70afcc69966999290e6aaeda0d7071297f07
VladlinaS <108859782+VladlinaS@users.noreply.github.com>
2022-07-14 11:40:26 +0300

    Nubers counter  with "while"

---
+++ while.numbers.py
@@ -0,0 +1,17 @@
+
+start_n = int(input('Enter the 1st number: '))
+end_n   = int(input('Enter the 2nd number: '))
+n       = start_n
+
+while n < end_n:
+    print(n)
+    n += 1
+while n >= end_n:
+    print(n)
+    n -= 1
+    
+
+    
+
+
+   
