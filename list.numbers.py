0d14ce27f8d082ce247ab7dff69c4f400ce44915
VladlinaS <108859782+VladlinaS@users.noreply.github.com>
2022-08-04 11:02:36 +0300

    Work with +(res) and  for x in range (len)

---
+++ list.numbers.py
@@ -0,0 +1,13 @@
+
+clients_high_priority = ["Tob", "Pete"]
+clients               = ["John", "Marry", "Kate"]
+clients_low_priority  = ["Vicky","Lessly"]
+res = clients_high_priority + clients + clients_low_priority
+print("Total clients: " + str(res))
+
+
+for c in range (len(res)): 
+    print(c, res [c])
+
+
+    
